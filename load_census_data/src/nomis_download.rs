844cebae2b0e695172f074c28fae15ab45b16ed6
Sam <a>
2021-11-29 21:10:25 +0000

    Added license header

--- load_census_data/src/nomis_download.rs
+++ load_census_data/src/nomis_download.rs
@@ -1,3 +1,23 @@
+/*
+ * Epidemic Simulation Using Census Data (ESUCD)
+ * Copyright (c)  2021. Sam Ralph
+ *
+ * This file is part of ESUCD.
+ *
+ * ESUCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * ESUCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ESUCD.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 //! Module used for download Census Tables from the NOMIS API
 use std::io::Write;
 use std::time::Instant;
--- load_census_data/src/tables/employment_densities.rs
+++ load_census_data/src/tables/employment_densities.rs
@@ -1,3 +1,23 @@
+/*
+ * Epidemic Simulation Using Census Data (ESUCD)
+ * Copyright (c)  2021. Sam Ralph
+ *
+ * This file is part of ESUCD.
+ *
+ * ESUCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * ESUCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ESUCD.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 /// This stores the area in square metres per full time employee
 ///
 /// Page 9 of Employment Densities Guide: 2nd Edition: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/378203/employ-den.pdf
