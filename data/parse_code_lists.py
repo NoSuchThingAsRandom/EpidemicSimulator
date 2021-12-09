63f67ac37900f9b19e6fc3886f0351ce8c82d990
Sam <a>
2021-12-09 19:08:05 +0000

    New workplace codes

--- data/parse_code_lists.py
+++ data/parse_code_lists.py
@@ -33,7 +33,29 @@ if __name__ == "__main__":
     print(len(codes))
     print(min(codes))
     print(max(codes))
-    print((list(find_ranges(codes))))
+    codes = sorted(list(codes))
+    # codes=((list(find_ranges(codes))))
+    current_code_start = codes[0]
+    current_code_end = codes[0]
+    new_codes = []
+    for code in codes:
+        if current_code_end + 50 < code:
+            new_codes.append((current_code_start, current_code_end))
+            current_code_start = code
+            current_code_end = code
+        else:
+            current_code_end = code
+    new_codes.append((current_code_start, current_code_end))
+    print(new_codes)
+    print(len(new_codes))
+    old = new_codes[0][1]
+    out = ""
+    for code in new_codes:
+        out += str(code[0]) + "..." + str(code[1]) + ","
+        print(code[0] - old)
+        old = code[1]
+    print(out)
+
     # print([list(group) for group in mit.consecutive_groups(codes)])
     # print(list(gb))
     # all_groups = ([i[1] for i in g] for _, g in gb)
