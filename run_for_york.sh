d34000c1cb4255199661224fc68ec314f2f2faa6
Sam <a>
2022-05-02 17:35:37 +0100

    Supports multiple runs

--- run_for_york.sh
+++ run_for_york.sh
@@ -1,12 +1,17 @@
 export RUST_LOG="warn,visualisation,osm_data=trace,sim=trace,run=debug,load_census_data=trace,voronoice=off"
 export RAYON_NUM_THREADS=40
 export RUST_BACKTRACE=full
-version="v1.7.2"
+version="v1.7.5"
 machine="workstation"
 area="1946157112TYPE299"
 full_path="statistics_results/"$machine"/"$version"/"$area"/"
-mkdir -p full_path
-echo "Saving results to: '"$full_path"'"
-cargo run --release -- $area --directory=data --grid-size=250000 --use-cache --simulate --output_name=$full_path 2>&1 | tee $full_path"log.log"
+for index in {1..5}
+do
+  output=$full_path$index"/"
+  mkdir -p $output
+  echo "Saving results to: '"$output"'"
+  cargo run --release -- $area --directory=data --grid-size=250000 --use-cache --simulate --output_name=$output 2>&1 | tee $output"log.log"
+done
+
 #2013265923TYPE299
 #1946157112TYPE299
